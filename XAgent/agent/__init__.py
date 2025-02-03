18d123e59476da9f1eb713e91ab3edb8496712ef
Robert <871607149@qq.com>
2025-02-03 18:41:21 -0500

    Update __init__.py

--- XAgent/agent/__init__.py
+++ XAgent/agent/__init__.py
@@ -1,6 +1,5 @@
 from .plan_generate_agent import PlanGenerateAgent
 from .plan_refine_agent import PlanRefineAgent
-from .tool_agent import ToolAgent
 from .reflect_agent import ReflectAgent
 from .father_reflect_agent import FatherReflectAgent
 from .simple_agent import SimpleAgent
